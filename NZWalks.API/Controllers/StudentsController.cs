9db0d7b16b558894d86ef4c32cee70e15ac2ad9e
Heshan Jayasundara - IT21295492 <heshanjayasundara218@gmail.com>
2024-05-14 22:12:22 +0530

    Add project files.

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    //https://loacalhost:portnumber/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        //Get: https://loacalhost:portnumber/api/students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "John", "Jane", "Mark", "Emily", "David" };
+
+            return Ok(studentNames);
+        }
+    }
+}
